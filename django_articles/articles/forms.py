32babeca8d734faab7fe7d428a7177f619c1b3f4
Nikita-Goncharov <nikitagoncarov657@gmail.com>
2023-02-09 11:04:26 +0200

    add login and register styles

--- django_articles/articles/forms.py
+++ django_articles/articles/forms.py
@@ -2,19 +2,19 @@ from django import forms
 
 
 class ArticleForm(forms.Form):
-    title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form_article_title'}))
-    body = forms.CharField(widget=forms.Textarea(attrs={'class': 'form_article_body'}))
+    title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form_article_title', 'placeholder': 'Title'}))
+    body = forms.CharField(widget=forms.Textarea(attrs={'class': 'form_article_body', 'placeholder': 'Your text'}))
 
 
 class LoginForm(forms.Form):
-    name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': ''}))
-    password = forms.CharField(widget=forms.PasswordInput())
+    name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form_name', 'placeholder': 'Name'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form_password', 'placeholder': 'Password'}))
 
 
 class RegisterForm(forms.Form):
-    name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': ''}))
-    password = forms.CharField(widget=forms.PasswordInput())
-    password_repeat = forms.CharField(widget=forms.PasswordInput())
+    name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form_name', 'placeholder': 'Name'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form_password', 'placeholder': 'Password'}))
+    password_repeat = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form_password_repeat', 'placeholder': 'Repeat password'}))
 
 
 class CommentForm(forms.Form):
